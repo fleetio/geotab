0f0763851d8c8ed79a234a4d5bc5685856edfaa2
Jorge Valdivia <jvaldivia@rarestep.com>
2015-09-23 14:41:00 -0500

    allow different connection paths

--- lib/geotab/client.rb
+++ lib/geotab/client.rb
@@ -1,16 +1,17 @@
 module Geotab
   class Client
-    AUTHENTICATION_URL = "https://my.geotab.com/apiv1/Authenticate"
+    DEFAULT_PATH = "my.geotab.com"
 
-    def authenticate(username, password, database=nil)
-      response = Faraday.get(AUTHENTICATION_URL,
+    def authenticate(username, password, database=nil, custom_path)
+      @custom_path = custom_path
+      response = Faraday.get(authentication_url,
                              {userName: username, password: password, database: database})
       result = JSON.parse(response.body)
 
       if result.has_key?("error")
         raise IncorrectCredentialsError, result["error"]["errors"].first["message"]
       else
-        @path = result["result"]["path"]
+        set_path(result["result"]["path"])
         set_credentials(result["result"]["credentials"].merge("path" => @path))
       end
     end
@@ -27,6 +28,22 @@ module Geotab
 
     def path
       @path
+    end
+
+    def authentication_url
+      "https://#{initial_path}/apiv1/Authenticate"
+    end
+
+    def initial_path
+      @custom_path || DEFAULT_PATH
+    end
+
+    def set_path path
+      if path == "ThisServer"
+        @path = initial_path
+      else
+        @path = path
+      end
     end
   end
 end
